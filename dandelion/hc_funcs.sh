eea37efc7ff91087b9d145bab82f3c144742bb66
samlee <samuelwjlee@gmail.com>
2021-03-05 09:32:01 -0800

    Add create_email_preview shortcut

--- dandelion/hc_funcs.sh
+++ dandelion/hc_funcs.sh
@@ -72,3 +72,12 @@ start() {
   ensure_npm_packages_up_to_date
   npm run start
 }
+
+create_email_preview() {
+  path_to_repo="$(pwd)"
+  if [ path_to_repo == *"consumer-api"* ] then
+    go run ./cmd/emailpreview/main.go
+  else
+    echo "Invalid repo. Please cd into consumer-api."
+  fi
+}
